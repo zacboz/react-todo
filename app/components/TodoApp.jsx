143a5ba1f83b4540482ff642ea5a92d264975beb
zacboz <zacboz@gmail.com>
2017-07-08 20:37:13 -0600

    refactor TodoSearch to work with Redux store

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -3,7 +3,7 @@ var uuid = require('node-uuid');
 var moment = require('moment');
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
-var TodoSearch = require('TodoSearch');
+import TodoSearch from 'TodoSearch';
 var TodoAPI = require('TodoAPI')
 
 var TodoApp = React.createClass({
