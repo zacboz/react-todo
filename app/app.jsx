4d41670b6639930b0d7598a044004ea8579377d2
zacboz <zacboz@gmail.com>
2017-07-08 23:09:52 -0600

    refactor app to work with localStorage

--- app/app.jsx
+++ app/app.jsx
@@ -1,15 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router'); // = require('react-router').Route
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var TodoApp = require('TodoApp');
 
 var actions = require('actions');
 var store = require('configureStore').configure();
+var TodoAPI = require('TodoAPI');
 
 store.subscribe(() => {
-  console.log('New State', store.getState());
+  var state = store.getState();
+  console.log('New State', state);
+  TodoAPI.setTodos(state.todos);
 });
+
+var initialTodos = TodoAPI.getTodos();
+store.dispatch(actions.addTodos(initialTodos));
 
 //Load foundation
 $(document).foundation();
