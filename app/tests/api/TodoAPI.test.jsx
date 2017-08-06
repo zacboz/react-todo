9124341692c0842d628d39c97607587f1dd8a93e
zacboz <zacboz@gmail.com>
2017-08-06 11:36:28 -0600

    fetch todos from firebase on app start

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -11,48 +11,48 @@ describe('TodoAPI', () => {
     expect(TodoAPI).toExist();
   })
 
-  describe('setTodos', () => {
-    it('should set valid todos array', () => {
-      var todos = [{
-        id: 23,
-        test: 'test all files',
-        completed: false
-      }];
-      TodoAPI.setTodos(todos);
+  // describe('setTodos', () => {
+  //   it('should set valid todos array', () => {
+  //     var todos = [{
+  //       id: 23,
+  //       test: 'test all files',
+  //       completed: false
+  //     }];
+  //     TodoAPI.setTodos(todos);
+  //
+  //     var actualTodos = JSON.parse(localStorage.getItem('todos'));
+  //
+  //     expect(actualTodos).toEqual(todos);//compares
+  //   });
+  //
+  //   it('should not set invalid todos array', () => {
+  //     var badTodos = {a: 'b'};
+  //     TodoAPI.setTodos(badTodos);
+  //
+  //     expect(localStorage.getItem('todos')).toBe(null);
+  //   })
+  // });
 
-      var actualTodos = JSON.parse(localStorage.getItem('todos'));
-
-      expect(actualTodos).toEqual(todos);//compares
-    });
-
-    it('should not set invalid todos array', () => {
-      var badTodos = {a: 'b'};
-      TodoAPI.setTodos(badTodos);
-
-      expect(localStorage.getItem('todos')).toBe(null);
-    })
-  });
-
-  describe('getTodos', () => {
-    it('should return empty array for bad localStorage data', () => {
-      var actualTodos = TodoAPI.getTodos();
-
-      expect(actualTodos).toEqual([]);
-    });
-
-    it('should return todos if valid array in localStorage', () => {
-      var todos = [{
-        id: 23,
-        test: 'test all files',
-        completed: false
-      }];
-
-      localStorage.setItem('todos', JSON.stringify(todos));
-      var actualTodos = TodoAPI.getTodos();
-
-      expect(actualTodos).toEqual(todos);
-    })
-  });
+  // describe('getTodos', () => {
+  //   it('should return empty array for bad localStorage data', () => {
+  //     var actualTodos = TodoAPI.getTodos();
+  //
+  //     expect(actualTodos).toEqual([]);
+  //   });
+  //
+  //   it('should return todos if valid array in localStorage', () => {
+  //     var todos = [{
+  //       id: 23,
+  //       test: 'test all files',
+  //       completed: false
+  //     }];
+  //
+  //     localStorage.setItem('todos', JSON.stringify(todos));
+  //     var actualTodos = TodoAPI.getTodos();
+  //
+  //     expect(actualTodos).toEqual(todos);
+  //   })
+  // });
 
   describe('filterTodos', () => {
     var todos = [{
