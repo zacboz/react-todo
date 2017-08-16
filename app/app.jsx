3edc713d63443b4b99e5ab8b0e0c3bf7f23ce76f
zacboz <zacboz@gmail.com>
2017-08-15 21:10:00 -0600

    update todos location to include uid

--- app/app.jsx
+++ app/app.jsx
@@ -11,23 +11,13 @@ import router from "app/router/";
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     store.dispatch(actions.login(user.uid));
+    store.dispatch(actions.startAddTodos());
     hashHistory.push("/todos");
   } else {
     store.dispatch(actions.logout());
     hashHistory.push("/");
   }
 });
-
-// store.subscribe(() => {
-//   var state = store.getState();
-//   console.log('New State', state);
-//   TodoAPI.setTodos(state.todos);
-// });
-
-// var initialTodos = TodoAPI.getTodos();
-// store.dispatch(actions.addTodos(initialTodos));
-
-store.dispatch(actions.startAddTodos());
 
 //Load foundation
 $(document).foundation();
